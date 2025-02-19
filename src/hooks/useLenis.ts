78a1e1fd3d5da74c1775a41b7b9a3469887da3e4
TheoDieppois <theo76.dieppois@gmail.com>
2025-02-19 18:06:17 +0100

    feat: add useLenis hook for smooth scrolling
    - Introduced `useLenis` custom hook to integrate the `lenis` library for smooth scrolling functionality.

---
+++ src/hooks/useLenis.ts
@@ -0,0 +1,13 @@
+import Lenis from "lenis";
+import { useEffect } from "react";
+
+export const useLenis = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+};
