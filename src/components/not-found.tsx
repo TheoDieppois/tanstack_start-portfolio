6aed7c6b3a8bebc9ecbc3257418dbf9558f784d4
TheoDieppois <theo76.dieppois@gmail.com>
2025-03-07 19:03:43 +0100

    style: update NotFound component layout and styling
    - Enhanced the layout of the NotFound component for better user experience.
    - Replaced Button component with native button and simplified Link usage.
    - Updated text and styles to improve responsiveness and visual appeal.

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,18 +1,28 @@
 import { Link } from "@tanstack/react-router";
-import { Button } from "./ui/button";
 
 export function NotFound() {
   return (
-    <div className="space-y-2 p-2">
-      <p>The page you are looking for does not exist.</p>
-      <p className="flex flex-wrap items-center gap-2">
-        <Button type="button" onClick={() => window.history.back()}>
-          Go back
-        </Button>
-        <Button asChild variant="secondary">
-          <Link to="/">Home</Link>
-        </Button>
-      </p>
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-4 p-4">
+      <h1 className="font-acorn text-sarcele-300 text-center text-[clamp(5rem,40vw,33rem)] leading-none font-bold">
+        404
+      </h1>
+      <p className="text-sarcele-300 text-xl">Page non trouvée</p>
+      <div className="flex flex-wrap items-center gap-6">
+        <button
+          type="button"
+          onClick={() => window.history.back()}
+          className="border-sarcele-300 hover:bg-sarcele-300/10 text-sarcele-300 relative mt-3 cursor-pointer rounded-full border px-8 py-3 text-xl font-medium shadow-lg transition-all duration-300"
+        >
+          Retour
+        </button>
+
+        <Link
+          to="/"
+          className="border-sarcele-300 hover:bg-sarcele-300/10 text-sarcele-300 relative mt-3 rounded-full border px-8 py-3 text-xl font-medium shadow-lg transition-all duration-300"
+        >
+          Accueil
+        </Link>
+      </div>
     </div>
   );
 }
